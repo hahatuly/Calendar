d2fc97a39b8cddee92a831d7383aec8eddcd49bd
Mikhail Sorokin <mikleso@mail.ru>
2024-02-01 22:18:25 +0300

    полностью рабочая бронь

--- src/main/java/com/example/Calendar/controllers/BookingController.java
+++ src/main/java/com/example/Calendar/controllers/BookingController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.yaml.snakeyaml.events.Event;
 
 //import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -29,11 +31,23 @@ public class BookingController {
     }
     @PostMapping("/Booking_time")
     public String Booking_time_add(@RequestParam LocalDateTime startTime, @RequestParam LocalDateTime endTime, @RequestParam String text, Model model) {
-
         bookingBase events = new bookingBase(startTime, endTime, text);
-        bookingRep.save(events);
+        List<bookingBase> bookings = bookingRep.findAll();
+        boolean free = true;
+        for (bookingBase booking : bookings) {
+            var StartTime = booking.getStartTime();
+            var EndTime = booking.getEndTime();
+            var NewInsideOld = StartTime.isBefore(events.getStartTime()) && EndTime.isAfter(events.getStartTime());
+            var OldInsideNew = events.getStartTime().isBefore(StartTime) && events.getEndTime().isAfter(StartTime);
+            if (NewInsideOld || OldInsideNew) {
+                free = false;
+                break;
+            }
+        }
+        if (free) {
+            bookingRep.save(events);
+        }
         return "redirect:/";
-
     }
 
     @GetMapping("/Booking_time/{id}")
